d4accacc331aa53253ba8000582e7721f1bf4bcd
Jonas Hörström <Jonas.Horstrom@qlik.com>
2018-05-16 11:25:47 +0200

    Update with CSV writer + new key naming

--- testkey.py
+++ testkey.py
@@ -2,6 +2,10 @@
 # script by Alex Eames http://RasPi.tv  
 # http://RasPi.tv/how-to-use-interrupts-with-python-on-the-raspberry-pi-and-rpi-gpio-part-3  
 import RPi.GPIO as GPIO  
+import csv
+import datetime
+
+
 GPIO.setmode(GPIO.BCM)  
   
 # GPIO 23 & 17 set up as inputs, pulled up to avoid false detection.  
@@ -10,19 +14,31 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
-  
+
+with open('happiness.csv', 'w') as csvfile:
+    fieldnames = ['timestamp', 'happiness']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+
 # now we'll define two threaded callback functions  
 # these will run in another thread when our events are detected  
-def callback_24(channel):  
-    print "falling edge detected on 24"  
+def callback_happy(channel):  
+    with open('happiness.csv', 'a') as csvfile:
+        writer = csv.writer(csvfile)
+        time = datetime.datetime.now().isoformat()
+        writer.writerow([time, 'happy'])
+    print "Happy pressed"  
+
+def callback_content(channel):  
+    global writer
+    print "Content pressed"  
+    writer.writerow({'timestamp': time.time(), 'happiness': 'content'})
 
 
-def callback_17(channel):  
-    print "falling edge detected on 17"  
-
-
-def callback_23(channel):  
-    print "falling edge detected on 23"  
+def callback_sad(channel):  
+    global writer
+    print "Sad pressed"  
+    writer.writerow({'timestamp': time.time(), 'happiness': 'sad'})
   
 print "Make sure you have a button connected so that when pressed"  
 print "it will connect GPIO port 23 (pin 16) to GND (pin 6)\n"  
@@ -35,9 +51,9 @@ raw_input("Press Enter when ready\n>")
 # when a falling edge is detected on port 23, regardless of whatever   
 # else is happening in the program, the function my_callback2 will be run  
 # 'bouncetime=300' includes the bounce control written into interrupts2a.py  
-GPIO.add_event_detect(23, GPIO.FALLING, callback=callback_23, bouncetime=300)  
-GPIO.add_event_detect(17, GPIO.FALLING, callback=callback_17, bouncetime=300)  
-GPIO.add_event_detect(24, GPIO.FALLING, callback=callback_24, bouncetime=300)  
+GPIO.add_event_detect(23, GPIO.FALLING, callback=callback_sad, bouncetime=300)  
+GPIO.add_event_detect(17, GPIO.FALLING, callback=callback_content, bouncetime=300)  
+GPIO.add_event_detect(24, GPIO.FALLING, callback=callback_happy, bouncetime=300)  
   
 try:  
     raw_input("Press enter to end program\n>")  
